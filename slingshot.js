f5f166e920a45ee876cf4b99507a542fb6d04977
BROHINGamer <71891588+BROHINGamer@users.noreply.github.com>
2020-11-03 19:00:11 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,18 @@
+ class Slingshot{
+     constructor(bodyA,pointB){
+         var options={
+             bodyA:bodyA,
+             pointB:pointB,
+             stiffness:0.04,
+             length:10
+         }
+this.sling=Matter.Constraint.create(options)
+World.add(world,this.sling);
+     }
+     display(){
+var pointA=this.sling.bodyA.position;
+var pointB=this.pointB;
+strokeWeight(4);
+line(pointA.x,pointA.y,pointB.x,pointB.y);
+     }
+ }
